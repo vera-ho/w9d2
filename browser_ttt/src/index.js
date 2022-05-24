5ba62f455711847328e41cb18921ba22dbecee90
Madeleine Pla <madeleine.pla7@gmail.com>
2022-05-24 15:12:21 -0400

    Add board

--- browser_ttt/src/index.js
+++ browser_ttt/src/index.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
-
+const View = require("./ttt-view.js");
+const Game = require("../ttt_node/game.js")
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+
+  const el = document.getElementsByClassName("ttt");
+
+  const gv = new View(game, el);
+
 });
--- browser_ttt/src/ttt-view.js
+++ browser_ttt/src/ttt-view.js
@@ -1,7 +1,26 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+    console.log("view constructed")
+  }
 
-  setupBoard() {}
+  setupBoard() {
+
+    const board = document.createElement('ul');
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const li = document.createElement('li');
+        li.dataset.pos = JSON.stringify([i, j]);
+        li.dataset.value = 'X';
+        board.appendChild(li);
+      }
+    }
+    console.log("board set up");
+    this.el[0].appendChild(board);
+  }
   
   bindEvents() {}
 
